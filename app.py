183756cdee74478e0fbf5eea84fc74fb42b5c696
nasarali03 <nasarali1805@gmail.com>
2024-08-31 12:51:28 +0500

    Update CSS file path in script.js

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ from metpy.calc import wind_components
 from metpy.plots import StationPlot, sky_cover
 from metpy.plots import StationPlot, sky_cover, current_weather, pressure_tendency as pt_symbols
 from scipy.spatial import cKDTree
-from folium import GeoJson, Marker, DivIcon
-from folium.plugins import MarkerCluster
-import scipy as sp
-import json,os,random,folium,io
-from mpl_toolkits.basemap import maskoceans
+import io,os
 from datetime import datetime, timezone
+
 
 # Use the Agg backend for Matplotlib
 import matplotlib
@@ -33,217 +30,50 @@ def idw_interpolation(x, y, z, xi, yi, power=3, chunk_size=10000):
         zi[i:i + chunk_size] = np.sum(weights * z[indices], axis=1)
     return zi
 
-def contours_to_geojson(contour_set):
-    features = []
-    for level, segs in zip(contour_set.levels, contour_set.allsegs):
-        for seg in segs:
-            coords = [[pt[0], pt[1]] for pt in seg if pt[0] is not None and pt[1] is not None]
-            if len(coords) > 0:
-                label_position = random.choice(['start', 'middle', 'end'])
-                if label_position == 'start':
-                    label_coords = coords[0]
-                elif label_position == 'middle':
-                    mid_index = len(coords) // 2
-                    label_coords = coords[mid_index]
-                else:  # 'end'
-                    label_coords = coords[-1]
-                    
-                features.append({
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "LineString",
-                        "coordinates": coords
-                    },
-                    "properties": {
-                        "level": int(level),
-                        "label": int(level),
-                        "label_coords": label_coords,
-                        "path": coords
-                    }
-                })
-    return json.dumps({"type": "FeatureCollection", "features": features})
-
-
-def add_labels_to_map(map_obj, feature):
-    label_coords = feature['properties']['label_coords']
-    label = feature['properties']['label']
-    
-    # Add the label horizontally along the contour line
-    Marker(
-        location=[label_coords[1], label_coords[0]],
-        icon=DivIcon(html=f'<div class="pressure-labels" style="font-size: 10px; cursor: grab; width:28px; text-align: center; border-radius:8px; color: white; transform: rotate(0deg); background-color: gray;">{label}</div>')
-    ).add_to(map_obj)
-
-def style_function(feature):
-    return {
-        'color': 'black',
-        'weight': 1,
-        'opacity': 0.8
-    }
-
-# Load the CSV data
-try:
-    data_file = "E:/Synop Data/Decoded_Data/2024082903.csv"
-    data = pd.read_csv(data_file)
-except FileNotFoundError:
-    print("File not found.")
-except Exception as e:
-    print("An error occurred:", str(e))
-
-def generate_map():
-    print("Generating maps")
-    lats = data['Latitude'].values
-    lons = data['Longitude'].values
-    air_temp = data['air_temp'].values  
-    dew_point = data['dew_point'].values
-    wind_speed = data['wind_speed'].values
-    wind_dir = data['wind_direction'].values
-    cloud_cover = data['cloud_cover'].values
-    pressure = data['pressure_sea_level'].values
-    stations=data['Station_Name'].values
-    codes=data['WMO'].values.astype(int)
-    precep=data['precipitation24H'].values
-
-    valid_indices = ~np.isnan(air_temp)
-    valid_indices1 = ~np.isnan(pressure)
-    valid_lats = lats[valid_indices1]
-    valid_lons = lons[valid_indices1]
-    valid_temps = air_temp[valid_indices].astype(float)
-    valid_pressure = pressure[valid_indices1].astype(float)
-
-    min_temp = np.min(valid_temps)
-    max_temp = np.max(valid_temps)
-    temperature_range = np.arange(min_temp, max_temp , 10)
-    colors = [
-    "rgb(149, 137, 211)",  # -10°C
-    "rgb(150, 209, 216)",  # 0°C
-    "rgb(129, 204, 197)",  # 10°C
-    "rgb(103, 180, 186)",  # 20°C
-    "rgb(95, 143, 197)",   # 30°C
-    "rgb(80, 140, 62)",    # 40°C
-    "rgb(121, 146, 28)",   # 50°C
-    "rgb(171, 161, 14)",   # 60°C
-    "rgb(223, 177, 6)",    # 70°C
-    "rgb(243, 150, 6)",    # 80°C
-    "rgb(236, 95, 21)",    # 90°C
-    "rgb(190, 65, 18)",    # 100°C
-    "rgb(138, 43, 10)"     # 110°C
-    ]
-
-# Ensure the number of colors matches the temperature range
-    if len(temperature_range) > len(colors):
-        raise ValueError("Not enough colors for the temperature range. Please add more colors.")
-
-    color_stops = list(zip(temperature_range, colors[:len(temperature_range)]))
-
-    def create_temp_icon(temp):
-        color = color_stops[0][1]  # Default to the lowest color
-        for threshold, color_value in color_stops:
-            if temp >= threshold:
-                color = color_value
-            else:
-                break
-    
-        return f"""
-        <div class="icon-container" style="background-color: {color};">
-            <div style="font-size: 12px; margin-left: 5px;">{temp}&deg;</div>
-        </div>
-        """
-    m = folium.Map(location=[32, 72], zoom_start=6)
-    m._name='map'
-    m._id='id'
-
-# Create a MarkerCluster layer
-    marker_cluster = MarkerCluster(name="Wind Data").add_to(m)
-
-# Add stations to the cluster
-    for lat, lon, speed, angle, temp, dew, cloud,station,code,pre in zip(lats, lons, wind_speed, wind_dir, air_temp, dew_point, cloud_cover,stations,codes,precep):
-        if not np.isnan(temp):
-            icon_html = create_temp_icon(temp)
-            
-            popup_html = (
-        '<div>'
-        + '<b>Station (' + '{:.3f}'.format(lat) + ', ' + '{:.3f}'.format(lon) + ')</b><br>'
-        + 'Temp: ' + '{:.1f}'.format(temp) + '&deg;C<br>'
-        + 'Rain: ' + '{:.1f}'.format(pre) + 'mm<br>'
-        + 'Dew Point: ' + '{:.1f}'.format(dew) + '&deg;C<br>'
-        + 'Wind: ' + '{:.1f}'.format(speed) + ' knots<br>'
-        + '<button onclick="onMarkerClick(' + str(code) + ')" id="show-model-' + str(code) + '">Show Station Model</button>'
-        + '<div id="model' + str(code) + '"></div>'
-        + '</div>'
-    )
-
-
-            folium.Marker(
-                location=[lat, lon],
-                icon=folium.DivIcon(html=icon_html),
-                popup=folium.Popup(folium.Html(popup_html, script=True), max_width=800),
-                tooltip=station
-            ).add_to(m)
-
-
-
-    lat_arr = np.linspace(valid_lats.min(), valid_lats.max(), 500)
-    lon_arr = np.linspace(valid_lons.min(), valid_lons.max(), 500)
-    lat_grid, lon_grid = np.meshgrid(lat_arr, lon_arr)
-    lat_grid_flat, lon_grid_flat = lat_grid.flatten(), lon_grid.flatten()
-
-    pressure_grid_flat = idw_interpolation(valid_lons, valid_lats, valid_pressure, lon_grid_flat, lat_grid_flat)
-    pressure_grid = pressure_grid_flat.reshape(lat_grid.shape)
-
-    pressure_grid = sp.ndimage.gaussian_filter(pressure_grid, sigma=3)
-    
-    pressure_grid_masked = maskoceans(lon_grid, lat_grid, pressure_grid)
-
-    contours = plt.contour(lon_grid, lat_grid, pressure_grid_masked, levels=20)
-
-    contour_geojson = contours_to_geojson(contours)
-
-    geojson_layer = GeoJson(
-        contour_geojson,
-        name='contours',
-        style_function=style_function,
-        popup=None
-    ).add_to(m)
-
-    # Manually add labels to the map at varied positions
-    for feature in json.loads(contour_geojson)['features']:
-        add_labels_to_map(m, feature)
-
-# Add high and low-pressure labels
-    min_point = np.unravel_index(np.argmin(pressure_grid), pressure_grid.shape)
-    max_point = np.unravel_index(np.argmax(pressure_grid), pressure_grid.shape)
-
-    Marker(
-        location=[lat_grid[min_point], lon_grid[min_point]],
-        icon=DivIcon(html=f'<div style="font-size: 16px; color: red;">L</div>')
-    ).add_to(m)
-
-    Marker(
-        location=[lat_grid[max_point], lon_grid[max_point]],
-        icon=DivIcon(html=f'<div style="font-size: 16px; color: blue;">H</div>')
-    ).add_to(m)
-
+def read_data(time_stamp):
     try:
-        m.save("./templates/interactive_map.html")
-        print("Map saved successfully.")
+        data_file = f"Decoded_Data/{time_stamp}.csv"
+        data = pd.read_csv(data_file)
+        return data
+    except FileNotFoundError:
+        print("File not found.")
     except Exception as e:
-        print(f"Error saving map: {e}")
+        print("An error occurred:", str(e))
+
 
 @app.route("/")
 def home():
-    # template_path = "./templates/interactive_map.html"
-    # if not os.path.exists(template_path):
-    #     print("No file avaialbale")
-    #     generate_map()
-    return render_template("2024083000.html")
+    now = datetime.now(timezone.utc)
+
+    now = datetime.now(timezone.utc)
+    timestamp = now.strftime("%Y%m%d")
+    print(timestamp)
+    
+    return render_template(f"{timestamp}00.html")
+
+@app.route('/list_html_files')
+def list_html_files():
+    template_dir = os.path.join(app.root_path, 'templates')
+    html_files = [f for f in os.listdir(template_dir) if f.endswith('.html')]
+    return jsonify(html_files)
+
+@app.route('/<timestamp>')
+def serve_html(timestamp):
+    return render_template(f'{timestamp}.html')
+
+@app.route("/getfile",methods=['GET'])
+def get_timestamp_file():
+    timestamp = request.args.get('timestamp', type=int)
+    return render_template(f"{timestamp}.html")
 
 @app.route('/generate_svg', methods=['GET'])
 def generate_svg():
     station_id = request.args.get('code', type=int)
+    time_stamp = request.args.get('timestamp', type=int)
     print(station_id)
+    print(time_stamp)
      # Load the data
-    data = pd.read_csv(data_file)
+    data=read_data(time_stamp)
 
     # Drop duplicates
     data = data.drop_duplicates(subset='station_id')
@@ -318,49 +148,49 @@ def generate_svg():
     return svg_data
 
 
-@app.route('/get_station_data', methods=['GET'])
-def get_station_data():
-    lat = float(request.args.get('lat'))
-    lon = float(request.args.get('lon'))
-    print(lat,lon)
-    filtered_data = data.drop_duplicates(subset='station_id')
+# @app.route('/get_station_data', methods=['GET'])
+# def get_station_data():
+#     lat = float(request.args.get('lat'))
+#     lon = float(request.args.get('lon'))
+#     print(lat,lon)
+#     filtered_data = data.drop_duplicates(subset='station_id')
     
-    # Apply the same filtering for NaNs
-    valid_indices_temp = ~np.isnan(filtered_data['air_temp'])
-    valid_indices_pressure = ~np.isnan(filtered_data['pressure_station_level'])
-    valid_indices_wind = ~np.isnan(filtered_data['wind_speed']) & ~np.isnan(filtered_data['wind_direction'])
+#     # Apply the same filtering for NaNs
+#     valid_indices_temp = ~np.isnan(filtered_data['air_temp'])
+#     valid_indices_pressure = ~np.isnan(filtered_data['pressure_station_level'])
+#     valid_indices_wind = ~np.isnan(filtered_data['wind_speed']) & ~np.isnan(filtered_data['wind_direction'])
     
-    valid_indices = valid_indices_temp & valid_indices_pressure & valid_indices_wind
-    filtered_data = filtered_data[valid_indices]
+#     valid_indices = valid_indices_temp & valid_indices_pressure & valid_indices_wind
+#     filtered_data = filtered_data[valid_indices]
     
-    # Find the closest station to the provided lat/lon
-    distances = np.sqrt((filtered_data['Latitude'] - lat)**2 + (filtered_data['Longitude'] - lon)**2)
-    closest_index = np.argmin(distances)
-    closest_station = filtered_data.iloc[closest_index]
+#     # Find the closest station to the provided lat/lon
+#     distances = np.sqrt((filtered_data['Latitude'] - lat)**2 + (filtered_data['Longitude'] - lon)**2)
+#     closest_index = np.argmin(distances)
+#     closest_station = filtered_data.iloc[closest_index]
 
-    # Find the closest station
-    latitude=closest_station['Latitude']
-    longitude=closest_station['Longitude']
-    air_temp = closest_station['air_temp']
-    dew_point = closest_station['dew_point']
-    cloud_cover = closest_station['cloud_cover']
-    pressure = closest_station['pressure_station_level']
-    wind_speed = closest_station['wind_speed']
-    wind_dir = closest_station['wind_direction']
-    station_name = closest_station['Station_Name']
+#     # Find the closest station
+#     latitude=closest_station['Latitude']
+#     longitude=closest_station['Longitude']
+#     air_temp = closest_station['air_temp']
+#     dew_point = closest_station['dew_point']
+#     cloud_cover = closest_station['cloud_cover']
+#     pressure = closest_station['pressure_station_level']
+#     wind_speed = closest_station['wind_speed']
+#     wind_dir = closest_station['wind_direction']
+#     station_name = closest_station['Station_Name']
     
-    station_data = {
-        'latitude': latitude,
-        'longitude': longitude,
-        'temperature': air_temp,
-        'pressure': pressure,
-        'dew_point': dew_point,
-        'wind_speed': wind_speed,
-        'wind_direction': wind_dir,
-        'cloud_cover': cloud_cover
-    }
+#     station_data = {
+#         'latitude': latitude,
+#         'longitude': longitude,
+#         'temperature': air_temp,
+#         'pressure': pressure,
+#         'dew_point': dew_point,
+#         'wind_speed': wind_speed,
+#         'wind_direction': wind_dir,
+#         'cloud_cover': cloud_cover
+#     }
     
-    return jsonify(station_data)
+#     return jsonify(station_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,31 +2,48 @@
 var link = document.createElement("link");
 
 link.rel = "stylesheet";
-link.href = "./style.css";
+link.href = "../static/style.css";
 document.head.appendChild(link);
-
 function onMarkerClick(code, time_stamp) {
   console.log({ time_stamp });
   fetch(
-    "http://127.0.0.1:5000/generate_svg?code=" +
-      code +
-      "&timestamp=" +
-      time_stamp
+    `https://web-production-7bb07.up.railway.app/generate_svg?code=${code}&timestamp=${time_stamp}`
   )
     .then((response) => response.text())
     .then((data) => {
       console.log(data);
-      var element = document.getElementById("model" + code);
+      const element = document.getElementById(`model${code}`);
 
       if (element) {
         element.innerHTML = `<div class="svg-container">${data}</div>`;
       } else {
-        console.error("Element not found: station-model-" + code);
+        console.error(`Element not found: station-model-${code}`);
       }
     });
 }
+
 var timestampSelector = document.createElement("select");
 timestampSelector.className = "timestamp-selector";
-timestampSelector.innerHTML =
-  '<option value="2023050315">2023-05-03 15:00</option>';
 document.body.appendChild(timestampSelector);
+
+var currentTimestamp = window.location.pathname.split("/").pop();
+
+fetch("https://web-production-7bb07.up.railway.app//list_html_files")
+  .then((response) => response.json())
+  .then((htmlFiles) => {
+    htmlFiles.forEach((file) => {
+      var timestamp = file.replace(".html", "");
+      var option = document.createElement("option");
+      option.value = timestamp;
+      option.textContent = timestamp;
+      timestampSelector.appendChild(option);
+    });
+    if (currentTimestamp) {
+      timestampSelector.value = currentTimestamp;
+    }
+  });
+
+timestampSelector.addEventListener("change", function () {
+  var selectedTimestamp = this.value;
+  window.location.href = `https://web-production-7bb07.up.railway.app//${selectedTimestamp}`;
+});
